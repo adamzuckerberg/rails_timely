e6d025745a6882849d4ce6c638a209f24d150b76
adamzuckerberg <adam@adamzuckerberg.com>
2015-01-11 15:06:13 -0800

    Validates name with error message test

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,17 @@
 class Account < ActiveRecord::Base
 	has_many :account_entries
 
-	validates :name, presence: true, length: {in: 1..70}
+	validates :name, presence: true, 
+				length: {in: 1..70, message: "Please use a name that is less than 70 characters and has not already been used."},
+				uniqueness: true
+
+	validate :your_name_is_not_silly
+
+	def your_name_is_not_silly
+		if  name.include?("silly")
+		 	errors.add(:name, " is silly")
+		else
+		puts "Your name is not silly"
+		end
+	end
 end
--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,7 +1,11 @@
 class TimeEntry < AccountEntry
 	belongs_to :employee
+	validates :time, numericality: {greater_than: 0, less_than: 24}
 
 	def cost 
 		time * 34
 	end
 end
+
+
+
